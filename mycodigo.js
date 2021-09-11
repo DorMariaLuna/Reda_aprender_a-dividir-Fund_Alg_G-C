3811cfa53106887fa429a5896fb65b8487a12b8b
josedaniel100 <87946350+josedaniel100@users.noreply.github.com>
2021-09-10 21:04:13 -0500

    10/09/2021
    se agrego una tabla para evaluacion

--- mycodigo.js
+++ mycodigo.js
@@ -1,4 +1,10 @@
 
+<style> 
+  body {
+    background-color: aquamarine; 
+  }
+</style>
+
 {
   
   alert ("¡Hola bienvenido ¿Quieres aprender hoy?!");
@@ -8,4 +14,25 @@
   <input type=button value=atras onclick="history.go">
 }
 
-
+<center> 
+<br>
+<h1>evaluación</h1>
+<hr width="50%" size="15" noshade="noshade" color="greem" >
+  <br>
+  <table border="5">
+        <th>descricipcion</th> 
+        <th>valor</th>
+        <tbody> 
+           <tr> 
+             <td> 
+              digite la pregunta 
+            </td>
+            <td>
+              <imput type="text" name="preg" id="pregun"
+              placeholder="escribe aqui la pregunta"> 
+          
+            </td>
+           </tr>
+        </tbody>
+  </table>
+</center>
